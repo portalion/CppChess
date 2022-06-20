e87b96d499c7c3e3e4373ef96329178dfdc58693
Portalion <77tobik77@gmail.com>
2022-06-21 00:15:29 +0200

    add Background color, delete normal color (you can now only add with background)

--- ConsoleChess/src/Console.cpp
+++ ConsoleChess/src/Console.cpp
@@ -1,11 +1,11 @@
 #include "Console.h"
 #include <windows.h>
 
-void Console::setColor(Color color)
+void Console::setColor(Color color, Color background)
 {
 	HANDLE  hConsole;
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, static_cast<int>(color));
+	SetConsoleTextAttribute(hConsole,static_cast<int>(color) + 16 * static_cast<int>(background));
 }
 
 void Console::resetToDefault()
--- ConsoleChess/src/Console.h
+++ ConsoleChess/src/Console.h
@@ -4,11 +4,15 @@ namespace Console
 {
 	enum class Color
 	{
+		None = 0,
 		Black = 1,
 		White = 8,
+		Red = 4,
+		Yellow = 14,
+		Aqua = 11,
 		Default = 7,
 	};
 
-	void setColor(Color color);
+	void setColor(Color color, Color background = Color::None);
 	void resetToDefault();
 }
